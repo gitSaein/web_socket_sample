5c9e1e75c0efb87c12df18d2ecbbae522bf1e35f
saeinlee <gitsaein@gmail.com>
2021-09-08 23:48:05 +0900

    init

---
+++ web_socket_sample/src/test/java/com/example/web_socket_sample/WebSocketSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_socket_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
